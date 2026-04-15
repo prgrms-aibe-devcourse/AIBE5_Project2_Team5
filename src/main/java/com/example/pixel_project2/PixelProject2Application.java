fd8ddeb795be1dcba60d4ded46dca3585f850151
kws0315 <kjk10191@gmail.com>
2026-04-15 12:51:21 +0900

    초기 데이터 (미완)

---
+++ src/main/java/com/example/pixel_project2/PixelProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.pixel_project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelProject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixelProject2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pixel_project2/PixelProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pixel_project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
