53907dee4ab4d985e9f2bb6817458eb7fd7eb3f9
kws0315 <kjk10191@gmail.com>
2026-04-21 04:03:40 +0900

    feat: explore에서 디자이너의 포트폴리오갯수 count하는 코드 추가 - 26.04.21(화)

--- src/main/java/com/example/pixel_project2/explore/service/ExploreService.java
+++ src/main/java/com/example/pixel_project2/explore/service/ExploreService.java
@@ -1,7 +1,12 @@
 package com.example.pixel_project2.explore.service;
 
+import com.example.pixel_project2.explore.dto.DesignerPostCount;
 import com.example.pixel_project2.explore.dto.ExplorePolicyResponse;
+
+import java.util.List;
 
 public interface ExploreService {
     ExplorePolicyResponse getExplorePolicy();
+
+    List<DesignerPostCount> getDesignerListWithPostCount();
 }
