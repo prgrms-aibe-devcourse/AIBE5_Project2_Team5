5287452a0122fc8f69473e8e01688c9332b33280
kws0315 <kjk10191@gmail.com>
2026-04-21 10:26:07 +0900

    feat: 로그인, 인증 오류 수정 + 모든 ERD 시퀸스 생성 완료 - 26.04.21 (화)

--- src/main/java/com/example/pixel_project2/common/entity/User.java
+++ src/main/java/com/example/pixel_project2/common/entity/User.java
@@ -3,6 +3,7 @@ package com.example.pixel_project2.common.entity;
 import com.example.pixel_project2.common.entity.enums.UserRole;
 import com.example.pixel_project2.common.entity.enums.Provider;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
@@ -23,14 +24,17 @@ public class User extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq_generator")
     private Long id;
 
-    @Column(name = "email", nullable = false, length = 30, unique = true)
-    private String email;
+    @Column(name = "login_id", nullable = false, length = 30, unique = true)
+    private String loginId;
 
     @Column(name = "password", nullable = false)
     private String password;
 
     @Column(name = "nickname", nullable = false, length = 10)
     private String nickname;
+
+    @Column(name = "name")
+    private String name;
 
     @Column(name = "profile_image", length = 255)
     private String profileImage;
@@ -39,10 +43,17 @@ public class User extends BaseTimeEntity {
     @Column(name = "role", nullable = false)
     private UserRole role;
 
-    @Column(name = "provider")
+    @Builder.Default
+    @Column(name = "follow_count")
+    private Integer followCount = 0;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Provider provider;
+
+    @Column(length = 100)
+    private String providerId;
+
     @Column(name = "url", length = 255)
     private String url;
-
-
 }
